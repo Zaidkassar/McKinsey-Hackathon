55086d80558fdcdff8930819d1d222f9a226ab24
Zaid Kassar <zaidkassar@Zaids-MacBook-Pro.local>
2016-10-22 15:34:27 -0400

    vaccines taken fix

--- server.py
+++ server.py
@@ -79,6 +79,7 @@ def response():
         command = body[len(body)-1]
         if  command == "on" or command == "off":
             body = body[:len(body)-1]
+            body = body.join()
             if body not in vaccines:
                 message = '"' + body + '" is not recognized as a current reccomended disease'
             elif command == "on":
