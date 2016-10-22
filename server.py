e063c5feddad1e552e5e5986a0c2a0fc0c521dc5
Zaid Kassar <zaidkassar@Zaids-MacBook-Pro.local>
2016-10-22 12:52:10 -0400

    command names

--- server.py
+++ server.py
@@ -14,13 +14,13 @@ def response():
     resp = twiml.Response()
     from_number = request.values.get('From', None)
     body = request.values.get('Body', None).lower()
-    if body == "updates":
+    if body == "updates on":
         if from_number not in numbers:
             numbers.append(from_number)
             message = 'You have been signed up for updates'
         else:
             message = 'You are already signed up for updates'
-    elif body == "cancel":
+    elif body == "updates off":
         if from_number not in numbers:
             message = 'You are not regsitered for updates'
         else:
@@ -29,7 +29,7 @@ def response():
     elif body == 'vaccines':
         message = ("Vaccines Available:\n Fill this")
     elif body == "commands":
-        message ='Commands:\n "updates" to recieve updates on vaccines\n "vaccines" for available vaccines\n"cancel" to stop updates'
+        message ='Commands:\n "updates on" to recieve updates on vaccines\n "vaccines" for available vaccines\n"updates off" to stop updates'
     else:
         message = 'Command not recognized, message "commands" for commands'
 
