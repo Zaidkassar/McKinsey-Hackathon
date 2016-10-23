bfd183470422a0a6893749018b2e22e2047a2de7
Zaid Kassar <zaidkassar@Zaids-MacBook-Pro.local>
2016-10-22 21:05:36 -0400

    test

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ vaccines = ['Hand, Foot and Mouth Disease']
 number_values = {}
 ACCOUNT_SID = "ACa21cfc418e446b328b2a6e652e885cac"
 AUTH_TOKEN = "d67465f7552e7b382b5c6a9e30dc6ded"
-gmaps = googlemaps.Client(key='AIzaSyD2GX2U7Hwqe9Ar_8BuBzyU7V9u4emqxcw')
+
 
 @app.route('/notify', methods=["GET", "POST"])
 def notifications():
